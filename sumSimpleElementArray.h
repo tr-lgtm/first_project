26aa10efe7ebd4676ec398cd6ca5aa29ae111d5c
EngineerVokswagen <Engineer_Vokswagen@tutamail.com>
2025-03-18 10:17:58 +0300

    Исправил недостатки.

--- sumSimpleElementArray.h
+++ sumSimpleElementArray.h
@@ -1,23 +1,2 @@
-bool isSimple(int number)
-{
-    for (int i = 2; i < number; i++) {
-        if (number % i == 0) {
-            return false;
-        }
-    } 
-    return true;
-}
-
-
-int sumSimpleElementArray(int* array, int size)
-{
-int sum_simple{};
-for(int i{}; i < size; i++)
-{
-if(isSimple(array[i]))
-{
-sum_simple += array[i];
-}
-}
-return sum_simple;
-}
+bool isSimple(int number);
+int sumSimpleElementArray(int* array, int size);
