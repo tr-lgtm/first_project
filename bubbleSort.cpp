8a3db9a4016a17b848efce993e36179046b1ff42
tr-lgtm <prohors100@gmail.com>
2025-03-24 23:13:20 +0300

    исправлена функция bubbleSort

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,8 +1,22 @@
 template <typename T>
-void bubbleSort(T* array, int size) {
+bool SortByDescending(T numberOne,T numberTwo)
+{
+    return numberOne > numberTwo;
+}
+
+
+template <typename T>
+bool SortByAscending(T numberOne,T numberTwo)
+{
+    return numberOne < numberTwo;
+}
+
+template <typename T>
+void bubbleSort(T* array, int size, bool (*comparison)(T,T))
+{
     for (int i = 0; i < size - 1; ++i) {
         for (int j = 0; j < size - i - 1; ++j) {
-            if (array[j] > array[j + 1]) {
+            if (comparison(array[j],array[j+1])) {
                 std::swap(array[j],array[j+1]);
             }
         }
--- bubbleSort.h
+++ bubbleSort.h
@@ -1,2 +1,6 @@
 template <typename T>
 void bubbleSort(T*, int);
+template <typename T>
+bool SortByDescending(T,T);
+template <typename T>
+bool SortByAscending(T,T);
