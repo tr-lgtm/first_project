7149a813066ecdc33a04adb2108b03987f63d1a7
EngineerVokswagen <Engineer_Vokswagen@tutamail.com>
2025-03-25 09:16:44 +0300

    corrected the errors.

--- calculateAverage.cpp
+++ calculateAverage.cpp
@@ -2,8 +2,16 @@
 template<class Type>
 double CalculateAverage(Type *  array, int size)
 {
-double sum{0};
-    for(int i{indexMinElement(array, size) + 1 }; i < indexMaxElement(array, size); i++)
+    double sum{0};
+    int i{indexMinElement(array, size)};
+    int ii{indexMaxElement(array, size)};
+    if(ii < i)
+    {
+        int tmp = i;
+        i = ii;
+        ii = tmp;
+    }
+    for(int i+1; i < ii ; i++)
         {
             sum += array[i];
         }
